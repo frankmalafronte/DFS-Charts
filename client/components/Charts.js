b610e4f7be983ba49e06c0bb7bc5ea481f84a7bf
Frank <frankamalafronte@gmail.com>
2019-02-03 21:05:18 -0500

    dates are ordered

--- client/components/Charts.js
+++ client/components/Charts.js
@@ -13,7 +13,28 @@ class Player extends Component {
     const response = await Axios.get(
       `/api/players/${this.props.match.params.id}`
     )
-    this.setState({player: response.data.Name, games: response.data.games})
+    console.log(response)
+    const sortDateStrings = input => {
+      for (let i = 0; i < input.length; i++) {
+        let el = input[i]
+        let year = el.Date.split(' ')[3]
+        let month = el.Date.split(' ')[1]
+        let day = el.Date.split(' ')[2]
+        let date = month + ' ' + day + ' ' + year
+        el.Date = date
+      }
+      // console.log(response.data.games)
+      // console.log(input)
+      input.sort(function(a, b) {
+        return new Date(b.Date) - new Date(a.Date)
+      })
+      return input
+    }
+
+    this.setState({
+      player: response.data.Name,
+      games: sortDateStrings(response.data.games)
+    })
   }
   render() {
     return (
