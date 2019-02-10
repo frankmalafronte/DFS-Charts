07a6e8893248409beac71188635de76de9b4026f
Frank <frankamalafronte@gmail.com>
2019-02-09 21:52:48 -0500

    adjusted math for averages

--- client/components/Averages.js
+++ client/components/Averages.js
@@ -2,21 +2,28 @@ import React from 'react'
 
 const averageScore = props => {
   let output = 0
-
+  let numberOfGames = 0
   for (let i = 0; i < props.games.length; i++) {
     let game = parseInt(props.games[i].Score)
-    output = output + game
+    if (game > 0) {
+      output = output + game
+      numberOfGames++
+    }
   }
-  return Math.round(output / props.games.length)
+  return Math.round(output / numberOfGames)
 }
 
 const averageSalary = props => {
   let output = 0
+  let numberOfGames = 0
   for (let i = 0; i < props.games.length; i++) {
     let game = parseInt(props.games[i].Salary)
-    output = output + game
+    if (game > 0) {
+      output = output + game
+      numberOfGames++
+    }
   }
-  return Math.round(output / props.games.length)
+  return Math.round(output / numberOfGames)
 }
 
 const averages = props => {
